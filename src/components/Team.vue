0faccd774d582c1b68d36b0f140b436bd24ac0ee
callazar <callazar1234@gmail.com>
2023-01-31 14:38:23 +0700

    update, jan 31, 14:38

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -3,7 +3,33 @@
 </script>
 
 <template>
-  <main class="absolute font-semibold team">
-   <h1 class="tracking-widest">TEAM</h1>
+  <main class="absolute team">
+   <h1 class="font-semibold tracking-widest">TEAM</h1>
+   <div class="flex flex-row justify-between mt-2 mr-2 ">
+    <div>
+        <img class="flex flex-col w-32 h-32 mt-4 rounded-full" src="../img/O1.jpg" alt="">
+        <h1 class="mt-2 font-semibold tracking-wider text-center">EXECUTOR</h1>
+    </div>
+    <div>
+        <img class="w-32 h-32 mt-4 rounded-full" src="../img/O2.jpg" alt="">
+        <h1 class="mt-2 font-semibold tracking-wider text-center">LEADER</h1>
+    </div>
+    <div>
+        <img class="w-32 h-32 mt-4 rounded-full" src="../img/O3.jpg" alt="">
+        <h1 class="mt-2 font-semibold tracking-wider text-center">CO-LEADER</h1>
+    </div>
+    <div>
+        <img class="w-32 h-32 mt-4 rounded-full" src="../img/O4.jpg" alt="">
+        <h1 class="mt-2 font-semibold tracking-wider text-center">GUARANTEE</h1>
+    </div>
+    <div>
+        <img class="w-32 h-32 mt-4 rounded-full" src="../img/O5.jpg" alt="">
+        <h1 class="mt-2 font-semibold tracking-wider text-center">ADVISOR</h1>
+    </div>
+    <div>
+        <img class="w-32 h-32 mt-4 rounded-full" src="../img/P1.jpg" alt="">
+        <h1 class="mt-2 font-semibold tracking-wider text-center">TECHNICIAN</h1>
+    </div>
+   </div>
   </main>
 </template>
