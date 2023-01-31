02188b610c1a0b39de79d86a1037c70f40df8203
callazar <callazar1234@gmail.com>
2023-01-31 15:46:06 +0700

    update, jan 31, 15:45

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -5,30 +5,36 @@
 <template>
   <main class="absolute team">
    <h1 class="font-semibold tracking-widest">TEAM</h1>
-   <div class="flex flex-row justify-between mt-2 mr-2 ">
+   <div class="flex flex-row m-2 space-x-14 ">
     <div>
         <img class="flex flex-col w-32 h-32 mt-4 rounded-full" src="../img/O1.jpg" alt="">
-        <h1 class="mt-2 font-semibold tracking-wider text-center">EXECUTOR</h1>
+        <h1 class="mt-2 font-bold tracking-wider text-center">EXECUTOR</h1>
+        <h4 class="text-center uppercase">Sarah</h4>
     </div>
     <div>
         <img class="w-32 h-32 mt-4 rounded-full" src="../img/O2.jpg" alt="">
-        <h1 class="mt-2 font-semibold tracking-wider text-center">LEADER</h1>
+        <h1 class="mt-2 font-bold tracking-wider text-center">LEADER</h1>
+        <h4 class="text-center uppercase">Jessica</h4>
     </div>
     <div>
         <img class="w-32 h-32 mt-4 rounded-full" src="../img/O3.jpg" alt="">
-        <h1 class="mt-2 font-semibold tracking-wider text-center">CO-LEADER</h1>
+        <h1 class="mt-2 font-bold tracking-wider text-center">CO-LEADER</h1>
+        <h4 class="text-center uppercase">Linda</h4>
     </div>
     <div>
         <img class="w-32 h-32 mt-4 rounded-full" src="../img/O4.jpg" alt="">
-        <h1 class="mt-2 font-semibold tracking-wider text-center">GUARANTEE</h1>
+        <h1 class="mt-2 font-bold tracking-wider text-center">GUARANTEE</h1>
+        <h4 class="text-center uppercase ">Michael</h4>
     </div>
     <div>
         <img class="w-32 h-32 mt-4 rounded-full" src="../img/O5.jpg" alt="">
-        <h1 class="mt-2 font-semibold tracking-wider text-center">ADVISOR</h1>
+        <h1 class="mt-2 font-bold tracking-wider text-center">ADVISOR</h1>
+        <h4 class="text-center uppercase ">joseph</h4>
     </div>
     <div>
         <img class="w-32 h-32 mt-4 rounded-full" src="../img/P1.jpg" alt="">
-        <h1 class="mt-2 font-semibold tracking-wider text-center">TECHNICIAN</h1>
+        <h1 class="mt-2 font-bold tracking-wider text-center">TECHNICIAN</h1>
+        <h4 class="text-center uppercase ">Firu</h4>
     </div>
    </div>
   </main>
